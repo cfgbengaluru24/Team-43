ea7cc0caab16046787d3d77fa83c03c844703f81
Aryan-Chharia <aryan.chharia@gmail.com>
2024-07-27 22:26:09 +0530

    admin dashboard

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-=======
-  content: [],
->>>>>>> 55a04fb3e6de6fcf44220cebbc549fc5d26ce16f
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
 }
-=======
-}
-
->>>>>>> 55a04fb3e6de6fcf44220cebbc549fc5d26ce16f
