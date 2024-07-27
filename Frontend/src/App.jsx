81bb078bd292ad7bd5d2a8ea81733ae985ebcce8
pranshulpahwa10 <12212040@nitkkr.ac.in>
2024-07-27 22:39:09 +0530

    deleted app.css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Admin from './company/BaseCompany'
-import BaseCompany from './company/BaseCompany'
+
+
 function App() {
   const [count, setCount] = useState(0)
 
